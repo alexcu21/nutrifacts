c6afd21f7f0f006fc1553a704f60ff03b4be758b
Alex Cuadra <alexcu21@live.com>
2020-01-26 18:42:04 -0600

    creating funtions for nutrifacts card

---
+++ nutrifacts-functions.php
@@ -0,0 +1,75 @@
+<?php 
+
+
+/*
+ * print the Shortcode
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/nf_shortcode.php';
+
+add_action('activated_plugin','my_save_error');
+function my_save_error()
+{
+file_put_contents(dirname(__file__).'/error_activation.txt', ob_get_contents());
+}
+
+/*
+* adding a new template: single-pruducts.php
+*/
+
+add_filter('template_include', 'products_template', 50, 1);
+
+function products_template($template) {
+
+    
+    /* Checks for single template by post type */
+    if ( is_singular('products') && file_exists(plugin_dir_path(__FILE__) . 'templates/single-products.php')) {
+        $template = plugin_dir_path(__FILE__) . 'templates/single-products.php';
+    }
+
+    return $template;
+
+}
+
+/*
+* adding styles to single-pruducts.php
+*/
+
+function nf_frontend_styles(){
+    wp_enqueue_style('nutrifacts-style', plugins_url('assets/css/nutrifacts-style.css' , __FILE__ ) );
+
+}
+add_action('wp_enqueue_scripts', 'nf_frontend_styles');
+
+function nf_get_result($label, $fieldname1, $fieldname2){
+
+	$nutrition_facts = get_field('nutrition_data');
+	$result = $nutrition_facts[$fieldname1] + $nutrition_facts[$fieldname2];
+	$percentage_result = round( $result / $nutrition_facts['servings_size'] * 100);
+
+	echo '<div>';
+		echo '<p>';
+			echo $label .' '. $result . 'g';
+		echo '<p>';	
+		echo '<p>';
+			echo $percentage_result . '%';
+		echo '<p>';	
+	echo '<div>';
+
+}
+
+function nf_get_percentage($label_element, $fieldname){
+	$nutrition_facts = get_field('nutrition_data');
+
+	$element = isset($nutrition_facts[$fieldname]) ? $nutrition_facts[$fieldname] : '' ;
+
+	$percentage_element = round( $element / $nutrition_facts['servings_size'] * 100);
+
+	echo '<div>';
+		echo '<p>';
+			echo $label_element .' '. $element . 'g';
+		echo '<p>';	
+		echo '<p>';
+			echo $percentage_element . '%';
+		echo '<p>';	
+	echo '<div>';
+}
--- templates/single-products.php
+++ templates/single-products.php
@@ -23,6 +23,12 @@ get_header();
 
 		$nutrition_facts = get_field('nutrition_data');
 
+		/* testing 
+
+		$total_fat = $nutrition_facts['satured_fat'] + $nutrition_facts['trans_fat'];			
+		$total_fat_per = round( $total_fat / $nutrition_facts['servings_size'] * 100);
+
+		*/
 /*
 echo '<pre>';
 		echo var_dump($nutrition_facts);
@@ -31,15 +37,90 @@ echo '</pre>';
 
 	 ?>
 
+<div class="product">
 	
-		 <?php the_post_thumbnail('thumbnail'); ?>
+	<div class="product-category">
 		 <p>Categories: <?php echo $the_terms; ?></p>
-		 <p><?php the_content(); ?></p>
-		 <p class="brand">the brand is: <?php echo $brand_title; ?></p>
-		  <p>Rated with: <?php the_field('product_rating'); ?></p>
+	</div>
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail('thumbnail'); ?>
+	</div>
+	<div class="brand">
+		<p class="brand">Brand: <?php echo $brand_title; ?></p>
+	</div>
+	<div class="product-description">
+		<p><?php the_content(); ?></p>
+	</div>
 
+	<div class="rating">
+		<p>Rated: <?php the_field('product_rating'); ?>/10</p>
+	</div>
 
+	<div class="nutrition-wrapper">
+		<div class="nutrition-card">
+			<div class="nutrition-header">
+				<h3>Nutrition Facts</h3>
+				<p>Serving Size <?php echo $nutrition_facts['servings_size']; ?>g</p>	
+			</div>
+			<div class="nutrition-body">
+				<div class="calories">
+					<p>Amount Per Serving</p>
+					<p>Calories <?php echo $nutrition_facts['_calories']; ?></p>
+				</div>
+				<div class="percentage-values">
+					<p>% Daily Value*</p>
+					<div class="daily-value total_fat">
+						<?php nf_get_result('Total Fat', 'saturated_fat', 'trans_fat'); ?>
+					</div>
+					<div class="daily-value satured_fat">
+						<?php nf_get_percentage('Satured Fat', 'saturated_fat'); ?>
+					</div>	
 
+					<div class="daily-value trans_fat">
+						<?php nf_get_percentage('Trans Fat', 'trans_fat'); ?>
+					</div>
+
+					<div class="daily-value cholesterol">
+						<?php nf_get_percentage('Cholesterol', 'cholesterol'); ?>
+					</div>
+
+					<div class="daily-value sodium">
+						<?php nf_get_percentage('Sodium', 'sodium'); ?>
+					</div>
+
+					<div class="daily-value total_carbohidrate">
+						<?php nf_get_result('Total Carbohidrate', 'dietary_fiber', 'sugars'); ?>
+					</div>
+
+					<div class="daily-value dietary_fiber">
+						<?php nf_get_percentage('Dietary Fiber', 'dietary_fiber'); ?>
+					</div>
+					
+					<div class="daily-value sugars">
+						<?php nf_get_percentage('Dietary Fiber', 'sugars'); ?>
+					</div>
+
+					<div class="daily-value protein">
+						<?php nf_get_percentage('Protein', 'protein'); ?>
+					</div>
+
+					<div class="vitamins">
+						<div class="vit-row1">
+							<p>Vitamin A <?php echo $nutrition_facts['vitamin_a'];  ?></p>
+							<p>Vitamin C <?php echo $nutrition_facts['vitamin_c'];  ?></p>
+						</div>
+						<div class="vit-row2">
+							<p>Calcium <?php echo $nutrition_facts['Calcium'];  ?></p>
+							<p>Iron <?php echo $nutrition_facts['Iron'];  ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+</div>
 
 <?php endwhile;
 
